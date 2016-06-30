1153ba85bc629cf9114d6d01de904bd2849b4273
eladlavi <eladlavi1@gmail.com>
2016-06-30 21:02:08 +0300

    math functions

--- Lesson05/Lesson05/Program.cs
+++ Lesson05/Lesson05/Program.cs
@@ -10,7 +10,58 @@ namespace Lesson05
     {
         static void Main(string[] args)
         {
-            //test
+            int result = product(7, 3);
+            Console.WriteLine(result);
+        }
+
+        static int distance(int x, int y)
+        {
+            int small = x;
+            int large = y;
+            if (x > y)
+            {
+                small = y;
+                large = x;
+            }
+            int result = 0;
+            while (small + result < large)
+                result++;
+            return result;
+        }
+
+        static int product(int x, int y)
+        {
+            int small = x;
+            int large = y;
+            if (x > y)
+            {
+                small = y;
+                large = x;
+            }
+            int result = 0;
+            for(int i = 0; i < small; i++)
+                result += large;
+            return result;
+        }
+        static int quotient(int x, int y)
+        {
+            if (y == 0)
+                return -1;
+            int result = 0;
+            int sum = y;
+            while(sum <= x)
+            {
+                result++;
+                sum += y;
+            }
+            return result;
+        }
+
+        static int remainder(int x, int y)
+        {
+            if (y == 0)
+                return -1;
+            return distance(x, product(quotient(x, y), y));
         }
     }
 }
