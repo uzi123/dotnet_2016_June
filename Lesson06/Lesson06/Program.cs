7961ddee32b22ddfa118241b1b9b80b4be3141ef
eladlavi <eladlavi1@gmail.com>
2016-07-04 18:10:39 +0300

    Collection

---
+++ Lesson06/Lesson06/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson06
+{
+    class Collection
+    {
+        int[] nums;
+        int pos;
+
+        public Collection()
+        {
+            nums = new int[10];
+            pos = 0;
+        }
+
+        public void Add(int number)
+        {
+            if(pos == nums.Length)//no room!
+            {
+                int[] temp = new int[nums.Length * 2];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    temp[i] = nums[i];
+                }
+                nums = temp;
+            }
+            nums[pos++] = number;
+        }
+
+        public int Get(int index)
+        {
+
+            return 0;
+        }
+
+        public void Remove(int index)
+        {
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
