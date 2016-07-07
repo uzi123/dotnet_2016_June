3d2b3219256720c549579a7d93ded3f7516adedb
eladlavi <eladlavi1@gmail.com>
2016-07-07 18:36:33 +0300

    Create Collection (empty)

--- Lesson07/Lesson07/Program.cs
+++ Lesson07/Lesson07/Program.cs
@@ -6,6 +6,74 @@ using System.Threading.Tasks;
 
 namespace Lesson07
 {
+
+    class Collection
+    {
+        public Collection()
+        {
+
+        }
+
+        public Collection(int size)
+        {
+        }
+
+        public void Add(int number)
+        {
+
+        }
+
+
+        
+
+        public int Get(int index)
+        {
+            return 0;
+        }
+        //a e y z a m m
+        public void Remove(int index)
+        {
+            
+        }
+
+        public int Size()
+        {
+            return 0;
+        }
+
+        public int[] ToArray()
+        {
+            int[] temp = null;
+            
+            return temp;
+        }
+
+        public int IndexOf(int number)
+        {
+            return 0;
+        }
+
+
+
+        //a b c d 0
+        public void Insert(int number, int index)
+        {
+            
+        }
+
+        public void Set(int number, int index)
+        {
+
+        }
+
+        public bool Equals(Collection other)
+        {
+            
+            return true;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
