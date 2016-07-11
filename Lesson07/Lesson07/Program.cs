27a96aadcf8d60f50f7fccbffeaacee072683b01
eladlavi <eladlavi1@gmail.com>
2016-07-11 19:05:37 +0300

    link

--- Lesson07/Lesson07/Program.cs
+++ Lesson07/Lesson07/Program.cs
@@ -11,20 +11,28 @@ namespace Lesson07
     {
 
         Link anchor;
-        
+        Link last;
+        int count;
 
         public Collection()
         {
             anchor = new Link(0);
+            last = anchor;
+            count = 0;
         }
         
 
         public void Add(int number)
         {
+            /*
             Link last = anchor ;
             while (last.next != null)
                 last = last.next;
             last.next = new Link(number);
+            */
+            last.next = new Link(number);
+            last = last.next;
+            count++;
         }
 
 
@@ -32,39 +40,62 @@ namespace Lesson07
 
         public int Get(int index)
         {
+            if (index >= count || index < 0)
+                throw new Exception("index out of bounds");
             int counter = -1;
             Link link = anchor;
-            while(link.next != null && counter < index)
+            while(counter < index)
             {
                 link = link.next;
                 counter++;
             }
-            if (counter == index)
-                return link.value;
-            else
-                throw new Exception("index out of bounds!");
+            return link.value;
         }
         //a e y z a m m
         public void Remove(int index)
         {
-            
+            if (index >= count || index < 0)
+                throw new Exception("index out of bounds");
+            Link theOneBefore = anchor;
+            for (int i = 0; i < index; i++)
+                theOneBefore = theOneBefore.next;
+            if(index == count - 1)
+            {
+                last = theOneBefore;
+            }
+            theOneBefore.next = theOneBefore.next.next;
+            count--;
         }
 
         public int Size()
         {
-            return 0;
+            return count;
         }
 
         public int[] ToArray()
         {
-            int[] temp = null;
-            
+            int[] temp = new int[count];
+            Link link = anchor;
+            for (int i = 0; i < count; i++)
+            {
+                link = link.next;
+                temp[i] = link.value;
+            }
             return temp;
         }
 
         public int IndexOf(int number)
         {
-            return 0;
+            int index = -1;
+            Link link = anchor;
+            while(link.next != null)
+            {
+                link = link.next;
+                index++;
+                if (link.value == number)
+                    return index;
+            }
+            return -1;
         }
 
 
@@ -72,12 +103,30 @@ namespace Lesson07
         //a b c d 0
         public void Insert(int number, int index)
         {
-            
+            if (index == count - 1)
+            {
+                Add(number);
+                return;
+            }
+            if (index >= count || index < 0)
+                throw new Exception("index out of bounds");
+            Link theOneBefore = anchor;
+            for (int i = 0; i < index; i++)
+                theOneBefore = theOneBefore.next;
+            Link insertedLink = new Link(number);
+            insertedLink.next = theOneBefore.next;
+            theOneBefore.next = insertedLink;
+            count++;
         }
 
         public void Set(int number, int index)
         {
-
+            if (index >= count || index < 0)
+                throw new Exception("index out of bounds");
+            Link link = anchor;
+            for (int i = 0; i < index + 1; i++)
+                link = link.next;
+            link.value = number;
         }
 
         public bool Equals(Collection other)
